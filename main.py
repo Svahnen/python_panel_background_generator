5ab8280cfc83dfb65f3f0fe21c5b75cdec1ffe1a
Svahnen <svahn.jonny@gmail.com>
2022-08-18 09:51:59 +0200

    Crop to desired resolution

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from PIL import Image
 import sys
 
+final_width = 1920  # Change here to the desired width
+final_height = 1080  # Change here to the desired height
+
+
+# Dont touch anything below this line
 img = Image.open(sys.argv[1])
 
-new_width = img.size[0] / (img.size[1] / 1080)
-new_height = img.size[1] / (img.size[0] / 1920)
+new_width = img.size[0] / (img.size[1] / final_height)
+# new_height is strictly not needed here, could be inside the else statement, but I'm keeping it here for clarity
+new_height = img.size[1] / (img.size[0] / final_width)
 
 
-if new_width <= 1920:
-    print("Scaling down to fit 1920px width")
-    img.thumbnail((1920, new_height))
+if new_width <= final_width:
+    print("Scaling down to fit width")
+    img.thumbnail((final_width, new_height))
+    crop_height = (img.size[1] - final_height) / 2
+    img_crop = img.crop((0, crop_height, final_width,
+                        final_height + crop_height))
 else:
-    print("Scaling down to fit 1080px height")
-    img.thumbnail((new_width, 1080))
+    print("Scaling down to fit height")
+    img.thumbnail((new_width, final_height))
+    crop_width = (new_width - final_width) / 2
+    img_crop = img.crop(
+        (crop_width, 0, final_width + crop_width, final_height))
 
-# TODO: Add crop
-
-img.show()
+img_crop.show()
